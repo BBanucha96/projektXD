51aceeb87dea2d264a8821dc56a45f658ecd1ff6
mgjda <mateuszjerzygajda@gmail.com>
2020-09-27 18:09:19 +0200

    clean code structure

--- API/WebAPI/Controllers/UsersController.cs
+++ API/WebAPI/Controllers/UsersController.cs
@@ -1,16 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Cors;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
-    //[Route("api/[controller]")]
     [ApiController]
     [Route("[controller]")]
     public class UsersController : ControllerBase
