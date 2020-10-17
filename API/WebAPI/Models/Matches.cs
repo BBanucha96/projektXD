89f7f7323bba6700bf15dfaa231ed11e27e28a72
mgjda <mateuszjerzygajda@gmail.com>
2020-10-17 21:22:19 +0200

    feat: add Matches model

---
+++ API/WebAPI/Models/Matches.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Models
+{
+    [Table("Matches")]
+    public class Matches
+    {
+        public int MatchId { get; set; }
+        public int MatchLink { get; set; }
+        public int DataStart { get; set; }
+        public int DataEnd { get; set; }
+    }
+}
