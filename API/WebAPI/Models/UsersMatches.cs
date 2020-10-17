9c5b13381c689825b05ead290bfd40fa24f0e574
mgjda <mateuszjerzygajda@gmail.com>
2020-10-17 20:52:44 +0200

    feat: add UsersMatches model

---
+++ API/WebAPI/Models/UsersMatches.cs
@@ -0,0 +1,17 @@
+﻿using NuGet.Frameworks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Models
+{
+    [Table("UsersMatches")]
+    public class UsersMatches
+    {
+        public int MatchId { get; set; }
+        public int UserId { get; set; }
+        public int BetScoreTeamA { get; set; }
+        public int BetScoreTeamB { get; set; }
+        public int MatchScore { get; set; }
+        
+    }
+}
