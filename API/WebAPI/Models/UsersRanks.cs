f142d6d8e293d8c47195292e357ff3fa037c9ccc
mgjda <mateuszjerzygajda@gmail.com>
2020-10-17 20:56:36 +0200

    feat: add UsersRanks model

---
+++ API/WebAPI/Models/UsersRanks.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Models
+{
+    [Table("UsersRanks")]
+    public class UsersRanks
+    {
+        public int UserId { get; set; }
+        public int CurrentRank { get; set; }
+        public int LastRank { get; set; }
+    }
+}
