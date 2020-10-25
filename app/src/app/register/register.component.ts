d91a4d24b243db9a7a1c3b5f776be54da232c49c
bbanucha96 <bbanucha96@gmail.com>
2020-10-25 22:02:29 +0100

    feat: add main validation to register form, only visual changes

--- app/src/app/register/register.component.ts
+++ app/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,15 +9,41 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   @Output() formChange = new EventEmitter<boolean>();
+  registerForm: FormGroup;
+  submitted = false;
 
-  constructor() { }
-
-  registerForm: any;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.registerForm = this.fb.group({
+        emailAddress: ['', [
+          Validators.required,
+          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+        userName: ['', [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(30),
+          Validators.pattern('^[a-zA-Z0-9]+([_-]?[a-zA-Z0-9])*$')]],
+        password: ['', [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(50),
+          Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')]],
+        passwordRpt: ['', [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(50),
+          Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')]]
+    });
   }
 
-  onSubmit(): void{
+  get registerFormControl() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+    console.table(this.registerForm.value);
   }
 
   changeForm(): void{
